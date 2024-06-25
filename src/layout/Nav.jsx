138b795d976efdeb4a25977f9ee27e8a3bcc2142
ethandamian <ethandamian@ciencias.unam.mx>
2024-06-25 15:25:49 -0600

    Fix background notch section for iOS devices

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -3,7 +3,7 @@ import BarsIcon from "@/icons/BarsIcon"
 
 export default function Nav() {
     return (
-        <nav className=" bg-white flex justify-between h-[60px] items-center px-8 py-[14px] shadow-md sticky top-0 pt-safe">
+        <nav className=" bg-white flex justify-between h-[60px] items-center px-8 py-[14px] shadow-md sticky top-0">
             <FitByAnaLogo className="h-[45px] w-[45px]" />
             <BarsIcon className="h-[32px] w-[32px] stroke-textColor" />
 
