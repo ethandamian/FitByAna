b1be370e548d8e03baa8810677b2bad3148afe18
ethandamian <ethandamian@ciencias.unam.mx>
2024-06-25 14:48:28 -0600

    Fix White background in ios devices and logo to svg

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
 
     return (
         <footer className="h-[60px] bg-white flex items-center justify-center gap-2 custom-shadow-top">
-            <FitByAnaLogo />
-            <p className=" text-textColor font-bold text-[10px]">© FIT BY ANA 2024</p>
+            <FitByAnaLogo className="w-[45px] h-[45px]" />
+            <p className=" text-textColor font-bold text-[12px]">© FIT BY ANA 2024</p>
         </footer>
     )
 }
